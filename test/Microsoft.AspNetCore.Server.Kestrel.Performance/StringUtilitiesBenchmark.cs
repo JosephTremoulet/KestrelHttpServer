23dd02ed9884e2eb0810bb2d64dc5ff61ed955b0
Joseph Tremoulet <jotrem@microsoft.com>
2017-06-09 15:33:15 -0400

    Convert StringUtilitiesBenchmark
    With two TODOs:
     - This was set up to run one [Benchmark] as the baseline, and report the
       second relative to that; running via xunit-performance currently
       doesn't report the ratio.
     - Decreased the iteration count 1000x because this was taking an
       inordinate amount of time otherwise.

--- test/Microsoft.AspNetCore.Server.Kestrel.Performance/StringUtilitiesBenchmark.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.Performance/StringUtilitiesBenchmark.cs
@@ -1,17 +1,24 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using BenchmarkDotNet.Attributes;
+// using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Performance
 {
-    [Config(typeof(CoreConfig))]
+    [BenchmarkDotNet.Attributes.Config(typeof(CoreConfig))]
     public class StringUtilitiesBenchmark
     {
-        private const int Iterations = 500_000;
+        private const int Iterations = 500; // *******************TODO******************: figure out good count; was 500_000, but that's taking a very long time
 
-        [Benchmark(Baseline = true, OperationsPerInvoke = Iterations)]
+        // ***********TODO********************: This one is intended as a baseline, report the metrics as ratios to the ones from this.
+        [Microsoft.Xunit.Performance.Benchmark(InnerIterationCount = Iterations)]
+        public void UintToString_()
+        {
+            var obj = this; // new StringUtilitiesBenchmark();
+            Microsoft.Xunit.Performance.Benchmark.Iterate(() => obj.UintToString());
+        }
+        [BenchmarkDotNet.Attributes.Benchmark(Baseline = true, OperationsPerInvoke = Iterations)]
         public void UintToString()
         {
             var connectionId = CorrelationIdGenerator.GetNextId();
@@ -21,7 +28,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Performance
             }
         }
 
-        [Benchmark(OperationsPerInvoke = Iterations)]
+        [Microsoft.Xunit.Performance.Benchmark(InnerIterationCount = Iterations)]
+        public void ConcatAsHexSuffix_()
+        {
+            var obj = this; // new StringUtilitiesBenchmark();
+            Microsoft.Xunit.Performance.Benchmark.Iterate(() => obj.ConcatAsHexSuffix());
+        }
+        [BenchmarkDotNet.Attributes.Benchmark(OperationsPerInvoke = Iterations)]
         public void ConcatAsHexSuffix()
         {
             var connectionId = CorrelationIdGenerator.GetNextId();
